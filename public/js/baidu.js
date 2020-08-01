861488858310582cb48f6420355af8873661844f
cabbagelol <nickmiku@foxmail.com>
2020-08-02 01:19:49 +0800

    网站

---
+++ public/js/baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5596e3f1bcbe6f8e06a106371d4ab4e7";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
