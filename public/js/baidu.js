d664a23e92aec06380c77e364e4236dc325ff9ee
cabbagelol <nickmiku@foxmail.com>
2022-01-24 13:54:50 +0800

    rem v1

--- public/js/baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?5596e3f1bcbe6f8e06a106371d4ab4e7";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
