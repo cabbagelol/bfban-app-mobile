b1c06e72bd7f097848cc33a339676f0a2052345c
Cabbagelol <nickmiku@foxmail.com>
2023-03-29 04:23:27 +0800

    v 0.2.2+13

--- build apk.bat
+++ build apk.bat
@@ -1,1 +1,14 @@
-flutter build apk ./lib/main.prod.dart --release --obfuscate --split-debug-info=build/debug_info
+#
+# 下发打包
+#
+
+
+# Android
+# flutter build apk ./lib/main.prod.dart --release --obfuscate --split-debug-info=build/debug_info
+# (Google) :
+# flutter build appbundle ./lib/main.prod.dart --release --build-number 2
+
+# Ios
+# (Apple) :
+# flutter build ipa ./lib/main.prod.dart --release --build-number 2
+# xcrun altool --upload-app --type ios -f build/ios/ipa/*.ipa --apiKey your_api_key --apiIssuer your_issuer_id
