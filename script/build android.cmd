dd34814ce24b450858b8ca42351dbab977b5ac6f
Cabbagelol <nickmiku@foxmail.com>
2024-03-24 10:11:08 +0800

    v0.2.10+1

--- script/build android.cmd
+++ script/build android.cmd
@@ -1,8 +1,8 @@
 # Android
 # (Local Build apk.file) :
-flutter build apk lib/main.prod.dart --release --build-number 41
+flutter build apk lib/main.prod.dart --release --build-number 42
 # (Build Google aab.file) :
-flutter build appbundle lib/main.prod.dart --release --build-number 41
+flutter build appbundle lib/main.prod.dart --release --build-number 42
 
 cp -R build/app/outputs/flutter-apk/*.* installation/
 
